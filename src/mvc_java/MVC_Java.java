8fe140a4ce4a404a96530dabdc0e15735f919a4c
Daelox201 <kuimayor2@gmail.com>
2022-02-25 13:22:54 -0600

    Actualizacion de la base de datos

--- src/mvc_java/MVC_Java.java
+++ src/mvc_java/MVC_Java.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mvc_java;
 
 import Controlador.ControladorClientes;
@@ -12,40 +7,41 @@ import Modelo.MDB;
 import Vista.FrmClientes;
 import Vista.FrmSeguimientoClientes;
 import Vista.FrmVista;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author Stevia
  */
-public class MVC_Java {
+public class MVC_Java  {
 
-    /**
-     * @param args the command line arguments
-     */
+    private FrmClientes _view;
+    private FrmSeguimientoClientes _view2;
+    private FrmVista _view3;
+
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        MDB objModelo=new MDB();
-        FrmVista objVista=new FrmVista();
+
+        MDB objModelo = new MDB();
+        FrmVista objVista = new FrmVista();
         FrmClientes objClientes = new FrmClientes();
         FrmSeguimientoClientes objSeguimiento = new FrmSeguimientoClientes();
         ControladorMDB objController;
         ControladorClientes objControllerClientes;
         ControladorSeguimientoClientes objControllerSiguimiento;
-      
-        objController = new ControladorMDB(objVista,objModelo);
+
+        objController = new ControladorMDB(objVista, objModelo);
         objController.iniciar();
-        objVista.setVisible(true);
-        
+
         objControllerClientes = new ControladorClientes(objClientes, objModelo);
         objControllerClientes.iniciar();
         objClientes.setVisible(true);
+
+        objControllerSiguimiento = new ControladorSeguimientoClientes(objSeguimiento, objModelo);
+        objControllerSiguimiento.iniciar();
+        objSeguimiento.setVisible(true);
         
-          objControllerSiguimiento = new ControladorSeguimientoClientes(objSeguimiento, objModelo);
-          objControllerSiguimiento.iniciar();
-          objSeguimiento.setVisible(true);
-          
-        
-    }
-    
+}
+
 }
